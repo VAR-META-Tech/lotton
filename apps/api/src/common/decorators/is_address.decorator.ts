0788629bb6f0c0240b47904d21095548d559d6d1
Hoan Le Huu <hoanle396@gmail.com>
2024-08-21 14:22:11 +0700

    feat: update ton address

--- apps/api/src/common/decorators/is_address.decorator.ts
+++ apps/api/src/common/decorators/is_address.decorator.ts
@@ -1,6 +1,14 @@
 import type { ValidationArguments, ValidationOptions } from 'class-validator';
 import { registerDecorator } from 'class-validator';
-import { isAddress } from 'web3-validator';
+
+function isValidTONAddress(address: any) {
+  if (typeof address !== 'string' || address.length !== 66) return false;
+
+  if (!address.startsWith('0:')) return false;
+
+  const hexPattern = /^[0-9a-fA-F]{64}$/;
+  return hexPattern.test(address.slice(2));
+}
 
 export function IsAddress(validationOptions?: ValidationOptions) {
   return function (object: object, propertyName: string) {
@@ -10,8 +18,8 @@ export function IsAddress(validationOptions?: ValidationOptions) {
       propertyName: propertyName,
       options: validationOptions,
       validator: {
-        validate(value: any, args: ValidationArguments) {
-          return isAddress(value);
+        validate(value: any, _args: ValidationArguments) {
+          return isValidTONAddress(value);
         },
         defaultMessage(args: ValidationArguments) {
           return `${args.property} must be a valid wallet address`;
