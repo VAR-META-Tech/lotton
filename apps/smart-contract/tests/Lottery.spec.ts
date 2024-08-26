b2214ea7f4ee78d0b1dacff4b517c2e965e37f3e
hung dang <hungdang@var-meta.com>
2024-08-26 18:09:04 +0700

    create pool, buy ticket

--- apps/smart-contract/tests/Lottery.spec.ts
+++ apps/smart-contract/tests/Lottery.spec.ts
@@ -2,8 +2,11 @@ import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
 import { toNano } from '@ton/core';
 import { Lottery } from '../wrappers/Lottery';
 import '@ton/test-utils';
+import now = jest.now;
+import { sleep } from '@ton/blueprint';
 
 describe('Lottery', () => {
+    jest.setTimeout(30000)
     let blockchain: Blockchain;
     let deployer: SandboxContract<TreasuryContract>;
     let lottery: SandboxContract<Lottery>;
@@ -11,7 +14,7 @@ describe('Lottery', () => {
     beforeEach(async () => {
         blockchain = await Blockchain.create();
 
-        lottery = blockchain.openContract(await Lottery.fromInit(0n));
+        lottery = blockchain.openContract(await Lottery.fromInit());
 
         deployer = await blockchain.treasury('deployer');
 
@@ -39,44 +42,83 @@ describe('Lottery', () => {
         // blockchain and lottery are ready to use
     });
 
-    it('should increase counter', async () => {
-        const increaseTimes = 3;
-        for (let i = 0; i < increaseTimes; i++) {
-            console.log(`increase ${i + 1}/${increaseTimes}`);
+    it('should create pool', async () => {
+        const provider = await blockchain.treasury('wallet');
 
-            const increaser = await blockchain.treasury('increaser' + i);
+        const pool = await lottery.send(
+            provider.getSender(), {
+                value: toNano('0.05'),
+            },
+            {
+                $$type: 'PoolCreated',
+                ticketPrice: 1n,
+                initialRounds: 1n,
+                startTime: 1724511710n,
+                endTime: 1735511710n,
+                active: true,
+            }
+        );
+        expect(pool.transactions).toHaveTransaction({
+            from: provider.address,
+            to: lottery.address,
+            success: false,
+        });
+    });
 
-            const counterBefore = await lottery.getCounter();
+    it('should return current pool', async () => {
+        const getPool = await lottery.getCurrentPool();
+        console.log('getPool', getPool);
+        expect(getPool).toBe(1n);
+    });
 
-            console.log('counter before increasing', counterBefore);
+    it('should return current round is 1n', async () => {
+        const getRound = await lottery.getCurrentRound();
+        console.log('getRound', getRound);
+        expect(getRound?.roundId).toBe(1n);
+    });
 
-            const increaseBy = BigInt(Math.floor(Math.random() * 100));
+    it('should return current round is active', async () => {
+        const getRound = await lottery.getCurrentRound();
+        console.log('getRound', getRound);
+        expect(getRound?.active).toBe(true);
+    });
 
-            console.log('increasing by', increaseBy);
+    it('should return current round is not expired', async () => {
+        const getRound = await lottery.getCurrentRound();
+        console.log('getRound', getRound);
+        expect(getRound?.startTime).toBe(1724511710n);
+        expect(getRound?.endTime).toBe(1735511710n);
+    });
 
-            const increaseResult = await lottery.send(
-                increaser.getSender(),
-                {
-                    value: toNano('0.05'),
-                },
-                {
-                    $$type: 'Add',
-                    queryId: 0n,
-                    amount: increaseBy,
-                }
-            );
+    it('should return user balance is enough', async () => {
+        const provider = await blockchain.treasury('wallet');
+        const balance = await provider.getBalance();
+        console.log('balance', balance);
+        expect(balance).toBeGreaterThan(1n);
+    });
 
-            expect(increaseResult.transactions).toHaveTransaction({
-                from: increaser.address,
-                to: lottery.address,
-                success: true,
-            });
+    it('should buy ticket', async () => {
+        const provider = await blockchain.treasury('wallet');
+        const balance = await provider.getBalance();
+        console.log('balance', balance);
+        const ticket = await lottery.send(
+            provider.getSender(), {
+                value: toNano('1.05'),
+            },
+            {
+                $$type: 'BuyTicket',
+                poolId: 1n,
+                roundId: 1n,
+                quantity: 1n
+            }
+        );
+        const balance2 = await provider.getBalance();
+        console.log('balance2', balance2);
 
-            const counterAfter = await lottery.getCounter();
-
-            console.log('counter after increasing', counterAfter);
-
-            expect(counterAfter).toBe(counterBefore + increaseBy);
-        }
+        expect(ticket.transactions).toHaveTransaction({
+            from: provider.address,
+            to: lottery.address,
+            success: false,
+        });
     });
 });
