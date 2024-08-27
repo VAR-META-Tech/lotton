c92e131b48399b4cbc72f62eb85f4155b6075214
Huy Vo <huyvo@var-meta.com>
2024-08-27 10:00:15 +0700

    feat: delete pool

--- apps/admin/src/modules/PoolDetail/index.tsx
+++ apps/admin/src/modules/PoolDetail/index.tsx
@@ -1,33 +1,59 @@
 'use client';
 
-import { HStack, VStack } from "@/components/ui/Utilities";
-import FormWrapper from '@/components/ui/FormWrapper';
-import { TextField } from '@/components/ui/FormField';
-import { Button } from '@/components/ui/button';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
-import { poolDetailSchema, PoolDetailSchema } from './components/types';
-import { useRouter } from 'next/navigation';
-import { useParams } from 'next/navigation';
-import { useGetPoolDetail } from '@/apis/pool';
 import { useEffect } from 'react';
-import { DatePickerField } from '@/components/ui/FormField/DatePickerField';
-import { convertToTimestamp } from '@/lib/utils';
+import { useParams, useRouter } from 'next/navigation';
+import { useGetPoolDetail } from '@/apis/pool';
+import { useDeletePool } from '@/apis/pool/mutations';
+import { zodResolver } from '@hookform/resolvers/zod';
 import dayjs from 'dayjs';
+import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+
+import { ROUTES } from '@/lib/routes';
+import { convertToTimestamp } from '@/lib/utils';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
+import { TextField } from '@/components/ui/FormField';
+import { DatePickerField } from '@/components/ui/FormField/DatePickerField';
+import FormWrapper from '@/components/ui/FormWrapper';
+import { HStack, VStack } from '@/components/ui/Utilities';
+
+import { poolDetailSchema, PoolDetailSchema } from './components/types';
 
 export const PoolDetail = () => {
   const route = useRouter();
   const { id } = useParams();
-  const { data: poolDetail } = useGetPoolDetail(String(id),
-    { queryKey: ['/pool/detail', id], enabled: !!id }
-  );
+  const { data: poolDetail } = useGetPoolDetail(String(id), { queryKey: ['/pool/detail', id], enabled: !!id });
 
   const methods = useForm<PoolDetailSchema>({
     resolver: zodResolver(poolDetailSchema),
   });
 
-  const handleSubmit = () => {}
-  
+  const { mutate: deletePoolMutate } = useDeletePool();
+
+  const handleSubmit = () => {};
+
+  const handleDeletePool = () => {
+    deletePoolMutate(Number(id), {
+      onSuccess: () => {
+        route.push(ROUTES.POOL);
+        toast.success('Delete pool successful');
+      },
+      onError: (error) => {
+        toast.error(`${error.message}`);
+      },
+    });
+  };
+
   useEffect(() => {
     const now = new Date().getTime();
 
@@ -36,15 +62,15 @@ export const PoolDetail = () => {
       sequency: poolDetail?.data?.sequency || 0,
       currency: poolDetail?.data?.currency?.name || 'N/A',
       totalRound: poolDetail?.data?.totalRounds || 0,
-      upcomingRound: poolDetail?.data?.rounds?.filter((round) => convertToTimestamp(round.startTime) > now).length, 
+      upcomingRound: poolDetail?.data?.rounds?.filter((round) => convertToTimestamp(round.startTime) > now).length,
       startTime: dayjs(poolDetail?.data?.startTime),
       endTime: dayjs(poolDetail?.data?.endTime),
       match1: poolDetail?.data?.poolPrizes[0]?.allocation || 0,
       match2: poolDetail?.data?.poolPrizes[1]?.allocation || 0,
       match3: poolDetail?.data?.poolPrizes[2]?.allocation || 0,
       match4: poolDetail?.data?.poolPrizes[3]?.allocation || 0,
-    })
-  }, [poolDetail])
+    });
+  }, [poolDetail]);
 
   return (
     <VStack className="mx-10 mb-24 bg-white rounded-sm min-h-[12.5rem] px-24 py-12">
@@ -192,10 +218,42 @@ export const PoolDetail = () => {
               </div>
             </VStack>
 
-            <HStack className="w-full justify-end gap-6 mt-20">
-              <Button onClick={() => route.back()} variant="outline" className="min-w-28 min-h-8 rounded-sm border text-[#8B8B94] border-[#8B8B94]">
+            <HStack className='w-full justify-end gap-6 mt-20'>
+              <Button
+                type="button"
+                onClick={() => route.back()}
+                variant="outline"
+                className='min-w-28 min-h-8 rounded-sm border text-[#8B8B94] border-[#8B8B94]'
+              >
                 Cancel
               </Button>
+
+              <AlertDialog>
+                <AlertDialogTrigger asChild>
+                  <Button type="button" className="min-w-28 min-h-8 rounded-sm text-white bg-[#E85858]">
+                    Delete
+                  </Button>
+                </AlertDialogTrigger>
+
+                <AlertDialogContent className='!rounded-sm'>
+                  <AlertDialogTitle className='text-center text-2xl'>Confirm Delete</AlertDialogTitle>
+                  <AlertDialogDescription className='space-y-6 text-black my-6 text-base'>
+                    <p>Are you sure you want to delete this pool? Deleting this pool will permanently
+                    remove it and all associated data.</p>
+                    
+                    <p>This action  cannot be undone.</p>
+                  </AlertDialogDescription>
+                  <AlertDialogFooter className='gap-4'>
+                    <AlertDialogCancel className='min-w-28 min-h-4 rounded-sm border text-[#8B8B94] border-[#8B8B94]'>Discard</AlertDialogCancel>
+                    <AlertDialogAction
+                      className='min-w-28 min-h-4 rounded-sm text-white bg-[#81A95D]'
+                      onClick={handleDeletePool}
+                    >
+                      Confirm
+                    </AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
 
               {/* <Button
                 type="button"
