05d2cfdec84a89e1bd2a8733f8765f2308247826
Tu <tuphan694@gmail.com>
2024-08-30 18:00:03 +0700

    feat: balance ton

--- apps/game/src/modules/CheckPage/index.tsx
+++ apps/game/src/modules/CheckPage/index.tsx
@@ -1,7 +1,15 @@
+'use client';
+
+import { useAuth } from '@/hooks/useAuth';
+import ConnectWalletSection from '../WalletPage/components/ConnectWalletSection';
 import { CheckWinner } from './components/CheckWinner';
 import { PoolList } from './components/PoolList';
 
 export const CheckPage = () => {
+  const { isLoggedIn } = useAuth();
+
+  if (!isLoggedIn) return <ConnectWalletSection />;
+
   return (
     <div className="container py-10 pb-24 space-y-16">
       <CheckWinner />
